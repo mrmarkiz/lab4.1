bd42550c01e4c0a191cf4a64b96e8558731f800c
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-04 21:49:11 +0300

    Add Task3 class

--- lab4/Task3.cs
+++ lab4/Task3.cs
@@ -10,7 +10,65 @@ namespace lab4
     {
         public static void Run()
         {
+            Console.Write("Enter bounds: ");
+            int a, b;
+            try
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                if (input.Length < 2)
+                {
+                    throw new Exception("Uncorrect bounds");
+                }
+                a = int.Parse(input[0]);
+                b = int.Parse(input[1]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error collapsed: {ex.Message}");
+                return;
+            }
+            if (a > b)
+            {
+                int tmp = a;
+                a = b;
+                b = tmp;
+            }
+            int result = GuessNum.Game.Play(a, b);
+            Console.WriteLine($"So your number is {result}");
+        }
+    }
+    namespace GuessNum
+    {
+        class Game
+        {
+            public static int Play(int a, int b)
+            {
+                string answer;
+                int dif = (a + b) / 2;
+                int current_num = dif;
+                do
+                {
+                    Console.WriteLine($"Is this your number {current_num}?");
+                    answer = Console.ReadLine();
+                    if(answer =="no")
+                    {
+                        Console.WriteLine("Your number is bigger or smaller?");
+                        answer = Console.ReadLine();
+                        if (answer == "bigger")
+                        {
+                            dif = Convert.ToInt32((Math.Ceiling(dif / 2.0)));
+                            current_num += dif;
+                        }
+                        else if (answer == "smaller")
+                        {
+                            dif = Convert.ToInt32((Math.Ceiling(dif / 2.0)));
+                            current_num -= dif;
+                        }
+                    }
 
+                } while (answer != "yes");
+                return current_num;
+            }
         }
     }
 }
