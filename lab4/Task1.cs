a235d8382ddb29660b453ed0d69d1511dd56931a
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-04 20:15:52 +0300

    Adding 1-2 tasks

--- lab4/Task1.cs
+++ lab4/Task1.cs
@@ -8,22 +8,132 @@ namespace lab4
 {
     internal class Task1
     {
+        private static void ShowArray(int[] array)
+        {
+            foreach(int i in array)
+                Console.Write($"{i} ");
+            Console.WriteLine();
+        }
         public static void Run()
         {
-            Console.Write("Enter number: ");
-            int number;
+            Console.Write("Enter bounds: ");
+            int a, b;
             try
             {
-                number = int.Parse(Console.ReadLine());
-                if (number < 0)
-                    throw new Exception("'-' symbol wasn't allowed");
+                string[] input = Console.ReadLine().Split(' ');
+                if(input.Length < 2)
+                {
+                    throw new Exception("Uncorrect bounds");
+                }
+                a = int.Parse(input[0]);
+                b = int.Parse(input[1]);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error collapsed: {ex.Message}");
                 return;
             }
-            Console.WriteLine($"Result number: {number}");
+            if (a > b) 
+            {
+                int tmp = a;
+                a = b;
+                b = tmp;
+            }
+            Console.Write("Enter what number you need(odd, even, simple): ");
+            string choice = Console.ReadLine();
+            switch(choice)
+            {
+                case "odd":
+                    ShowArray(Fibonacci.Odd.Generator.Get(a, b));
+                    break;
+                case "even":
+                    ShowArray(Fibonacci.Even.Generator.Get(a, b));
+                    break;
+                case "simple":
+                    ShowArray(Fibonacci.Simple.Generator.Get(a, b));
+                    break;
+            }
+        }
+    }
+
+    namespace Fibonacci
+    {
+        namespace Even
+        {
+            class Generator
+            {
+                public static int[] Get(int a, int b)
+                {
+                    int[] res = new int[0];
+                    int x= 0, y = 1, z = 1;
+                    while (x <= b)
+                    {
+                        if (x >= a && x % 2 == 0)
+                        {
+                            res = res.Append(x).ToArray();
+                        }
+                        int tmp = z + y;
+                        x = y;
+                        y = z;
+                        z = tmp;
+                    }
+                    return res;
+                }
+            }
+        }
+        namespace Odd
+        {
+            class Generator
+            {
+                public static int[] Get(int a, int b)
+                {
+                    int[] res = new int[0];
+                    int x = 0, y = 1, z = 1;
+                    while (x <= b)
+                    {
+                        if (x >= a && x % 2 == 1)
+                        {
+                            res = res.Append(x).ToArray();
+                        }
+                        int tmp = z + y;
+                        x = y;
+                        y = z;
+                        z = tmp;
+                    }
+                    return res;
+                }
+            }
+        }
+        namespace Simple
+        {
+            class Generator
+            {
+                private static bool IsSimple(int a)
+                {
+                    int num = 0;
+                    for (int i = 1; i < Math.Sqrt(a); i++)
+                        if (a % i == 0)
+                            num++;
+                    return num <= 1;
+                }
+                public static int[] Get(int a, int b)
+                {
+                    int[] res = new int[0];
+                    int x = 0, y = 1, z = 1;
+                    while (x <= b)
+                    {
+                        if (x >= a && IsSimple(x))
+                        {
+                            res = res.Append(x).ToArray();
+                        }
+                        int tmp = z + y;
+                        x = y;
+                        y = z;
+                        z = tmp;
+                    }
+                    return res;
+                }
+            }
         }
     }
 }
---
+++ lab4/Task2.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Task2
+    {
+        public static void Run()
+        {
+            Console.Write("Enter what figure to print(triangle, square, rectangular): ");
+            string choice = Console.ReadLine();
+            int a, b;
+            switch(choice)
+            {
+                case "triangle":
+                    Console.Write("Enter side length: ");
+                    try
+                    {
+                        a = int.Parse(Console.ReadLine());
+                        if (a < 0)
+                            throw new Exception("Length must be positive number");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error collapsed: {ex.Message}");
+                        return;
+                    }
+                    Figures.Triangle.Printer.Print(a);
+                    break;
+
+                case "square":
+                    Console.Write("Enter side length: ");
+                    try
+                    {
+                        a = int.Parse(Console.ReadLine());
+                        if (a < 0)
+                            throw new Exception("Length must be positive number");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error collapsed: {ex.Message}");
+                        return;
+                    }
+                    Figures.Square.Printer.Print(a);
+                    break;
+
+                case "rectangular":
+                    Console.Write("Enter sides length: ");
+                    try
+                    {
+                        string[] input = Console.ReadLine().Split(' ');
+                        if (input.Length < 2)
+                            throw new Exception("Uncorrect length");
+                        a = int.Parse(input[0]);
+                        b = int.Parse(input[1]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error collapsed: {ex.Message}");
+                        return;
+                    }
+                    Figures.Rectangular.Printer.Print(a, b);
+                    break;
+            }
+        }
+    }
+
+    namespace Figures
+    {
+        namespace Triangle
+        {
+            class Printer
+            {
+                public static void Print(int a)
+                {
+                    for(int i = 0; i < a; i++)
+                    {
+                        for(int j=0;j<a-i;j++)
+                            Console.Write(" ");
+                        for (int j=0;j<=i;j++)
+                            Console.Write("* ");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+        namespace Square
+        {
+            class Printer
+            {
+                public static void Print(int a)
+                {
+                    for(int i=0; i<a; i++)
+                    {
+                        for(int j=0;j<a; j++)
+                            Console.Write("* ");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+        namespace Rectangular
+        {
+            class Printer
+            {
+                public static void Print(int a, int b)
+                {
+                    for(int i=0;i<a;i++)
+                    {
+                        for(int j=0;j<b;j++)
+                            Console.Write("* ");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
